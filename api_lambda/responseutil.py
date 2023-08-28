62e4f260ba8ee58584b6b70bea6e53c363b884bc
Azra Eiden <carter.eiden@gmail.com>
2023-08-28 04:19:40 -0400

    Fix

--- api_lambda/responseutil.py
+++ api_lambda/responseutil.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 
 def response_to_fastapi_response(some_json, status_code=200):
     if status_code >= 400:
-        raise HTTPException(some_json, status_code)
+        raise HTTPException(status_code, some_json)
     return some_json
 
 def error_response_to_fastapi_response(some_json, status_code=400):
