1c5b699db6611da6dc6b8d0cb6cd877d8bcb53b7
Azra Eiden <carter.eiden@gmail.com>
2023-08-31 11:53:10 -0400

    Authorization Working

--- README.md
+++ README.md
@@ -6,8 +6,18 @@ Serverless, lightweight, and fast vector store on top of DynamoDB
 
 Whiplash is a lightweight vector store built on top of [AWS DynamoDB](https://aws.amazon.com/dynamodb/). It uses a variant of [locality-sensitive hashing (LSH)](https://en.wikipedia.org/wiki/Locality-sensitive_hashing) to index vectors in a DynamoDB table. This is intended to be a mimimalist, scalable, and fast vector store and is intended to be extremely easy to use, maintain, and self-host.
 
-### Common Deployment Modifications
+## Deployment and Usage
+
+We recommend that you clone this Github template into a personal workspace or organization within Github.
+
+The API is deployed using [CloudKommand](https://cloudkommand.com), and it uses API Key authentication, which must be set in the project's secrets before deploying.
+
+The fundamental units in Whiplash are collections and items, where an item is a vector, and a collection is a set of items that are indexed for fast search. Default settings for collections
+are set during deployment, but all settings can be changed when creating a collection.
+
+### Common Deployment Modifications (Within CloudKommand)
 
  - To add a (pretty) domain name to the API, set "domain": "your_domain_name" in the apigw component
  - To change the region from the default set the CloudKommand project to be in a different region
- - To change the stage (dev vs qa vs prod) set the environment variable in the API Lambda
+ - To change the default values for collections, override the environment variables in the api_lambda component
+ - To change the stage (dev vs qa vs prod), set the STAGE environment variable in the api_lambda component
--- api_lambda/lambda_function.py
+++ api_lambda/lambda_function.py
@@ -4,7 +4,7 @@ import json
 import traceback
 from mangum import Mangum
 
-from fastapi import FastAPI, APIRouter, Depends, Request, HTTPException, Header, status, Body, Path
+from fastapi import FastAPI, APIRouter, Depends, Request, HTTPException, Header, status, Body, Path, Security
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.security.api_key import APIKeyHeader
 from starlette.middleware import Middleware
@@ -27,47 +27,44 @@ from projects import get as api_get_project, all as api_list_projects, \
 
 logger = Logger(service="whiplash-api")
 
+
+API_KEY_HEADER = APIKeyHeader(name="x-api-key", auto_error=False)
+
+def get_api_key(api_key_header: str = Security(API_KEY_HEADER)):
+    if api_key_header == os.environ.get("API_KEY"):
+        return api_key_header
+    else:
+        raise HTTPException(status_code=401, detail="Unauthorized, invalid or missing API Key")
+
 app = FastAPI(
     title="Whiplash API",
-    summary="CloudKommand-deployed standalone API that provides an easy-to-use apierface for self-hosted Whiplash, a serverless vector store.",
+    summary="CloudKommand-deployed standalone API that provides an easy-to-use api interface for self-hosted Whiplash, a serverless vector store.",
     description="""
-    
-    Include stuff about what a project is and what a collection is.
+        The fundamental units in Whiplash are collections and items, where an item is a vector, and a collection is a set of items that are indexed for fast search. Default settings for collections are set during deployment, but all settings can be changed when creating a collection.
     """,
     docs_url="/docs",
     root_path="/live",
-    version=os.environ.get("VERSION", "1.0.0")
+    version=os.environ.get("VERSION", "1.0.0"),
+    dependencies=[Depends(get_api_key)]
 )
 app.add_middleware(ExceptionMiddleware, handlers=app.exception_handlers)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+
 project_router = APIRouter()
+
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event, context):
+    return Mangum(app, api_gateway_base_path="/live")(event, context)
 
 @app.exception_handler(Exception)
 async def unhandled_exception_handler(request, err):
     logger.exception(f"Unhandled exception in {request.url.path}: {err}")
     return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
 
-def lambda_env(key):
-    return os.environ.get(key)
 
-@logger.inject_lambda_context(log_event=True)
-def lambda_handler(event, context):
-    try:
-        # table_name = lambda_env("table_name")
+
     
-        # dynamodb = boto3.client("dynamodb")
-
-        # context = event.get("requestContext")
-        # http_method = context.get("http").get("method")
-        # raw_path = event['rawPath']
-        # path = raw_path.replace("/live", "")
-        
-        return Mangum(app, api_gateway_base_path="/live")(event, context)
-
-    except:
-        print(traceback.format_exc())
-
 @project_router.get("/", tags=["Projects"])
 def get_projects() -> ProjectsOut:
     return api_list_projects()
@@ -109,9 +106,9 @@ def batch_create_items(project_name: str, collection_name: str, batch: BatchCrea
     batch = batch.dict()
     return api_batch_create_items(project_name, collection_name, batch["vectors"])
 
-@app.post("/initialize")
-def initialize() -> CreateSuccess:
-    return api_initialize()
+# @app.post("/initialize", tags=["Initialize"])
+# def initialize() -> CreateSuccess:
+#     return api_initialize()
 
 app.include_router(project_router, prefix="/projects")
 
